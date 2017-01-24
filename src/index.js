89e91962c2b2d1239f5f67857cb2b3bc42413863
mikeadossi <mikeadossi@gmail.com>
2017-01-24 11:00:32 -0800

    setup webpack, tested src/index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+let message = 'HELLO'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+console.log(message);
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+//
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
