e37f5d7eb7be7c6791a63cb28bda50caa6312c53
AdamB37 <adam.beshir@gmail.com>
2017-01-25 10:24:09 -0800

    commit

--- src/components/create-todo.js
+++ src/components/create-todo.js
@@ -12,8 +12,20 @@ export default class TodosListItem extends React.Component{
 
   handleCreate(event) {
     event.preventDefault();
+
+    const createInput = this.refs.createInput
+    const task = createInput.value
+    const validateInput = this.validateInput(task);
+
     this.props.createTask(this.refs.createInput.value);
     this.refs.createInput.value = '';
-    //console.log(this.refs.createInput.value);
+
+  }
+
+  validateInput(task) {
+    if(!task) {
+      return 'Please enter a task.'
+    }
+    if(_.find(this.props))
   }
 }
